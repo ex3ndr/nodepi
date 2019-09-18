c72647be8d06c8ed3964637e363a88de56bd4302
Steve Korshakov <steve@korshakov.com>
2019-09-17 22:33:14 -0700

    wip: Add logging to launcher

--- src/packages/launcher/index.ts
+++ src/packages/launcher/index.ts
@@ -7,6 +7,7 @@ import fs from 'fs';
 import rimraf from 'rimraf';
 
 // Init root directory
+console.log('Validate and prepare data dir')
 let root = (process.env.STORAGE_PATH || __dirname);
 if (!fs.existsSync(root)) {
     fs.mkdirSync(root);
@@ -32,6 +33,7 @@ if (fs.existsSync(root + '/modules/')) {
 fs.mkdirSync(root + '/modules/');
 
 // Write Packages
+console.log('Writing package index');
 fs.writeFileSync(root + '/modules/package.json', JSON.stringify({
     name: 'nodepi_modules',
     dependencies: {
@@ -40,4 +42,9 @@ fs.writeFileSync(root + '/modules/package.json', JSON.stringify({
 }));
 
 // Install packages
-child_process.execSync('yarn', { cwd: root + '/modules/' });
+console.log('Installing packages');
+child_process.execSync('yarn', { cwd: root + '/modules/', stdio: 'inherit' });
+
+// Run server
+console.log('Starting server');
+child_process.execSync('node "' + root + '/modules/node_modules/nodepi/build/server/index.js"', { cwd: root + '/modules/', stdio: 'inherit' });
